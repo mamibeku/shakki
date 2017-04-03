5da875cc0a4592a5c282e1ce02f012f27f561441
mamibeku <mamibeku@student.jyu.fi>
2017-04-03 11:32:03 +0300

    Kaikki siirrot toimii ehkä

--- PerusPeli1/PerusPeli1/Siirrot.cs
+++ PerusPeli1/PerusPeli1/Siirrot.cs
@@ -21,7 +21,7 @@ namespace Mscee
             // TODO: jokin virheteksti esim. "yritit tehdä laittoman siirron. huutista" samaan tyyliin kuin voittamisteksti
             if (vari) // on valkoinen
             {
-                if (uusiY < vanhaY || uusiY - vanhaY > 2)
+                if (uusiY <= vanhaY || uusiY - vanhaY > 2)
                 {
                     return true;
                 }
@@ -33,7 +33,7 @@ namespace Mscee
                 {
                     return true;
                 }
-                if (uusiY == 3 && peli.lauta.Lauta(uusiX, 2) != null && uusiX - vanhaX == 0)
+                if (uusiY == 3 && peli.lauta.Lauta(uusiX, 2) != null && vanhaY == 1)
                 {
                     return true;
                 }
@@ -41,15 +41,11 @@ namespace Mscee
                 {
                     return true;
                 }
-                if (uusiY - vanhaY == 0)
+                if (uusiX != vanhaX && uusiY - vanhaY == 2)
                 {
                     return true;
                 }
-                if (uusiX - vanhaX != 0 && uusiY - vanhaY == 2)
-                {
-                    return true;
-                }
-                if (uusiX - vanhaX != 0 && peli.lauta.Lauta(uusiX, uusiY) == null)
+                if (uusiX != vanhaX && peli.lauta.Lauta(uusiX, uusiY) == null)
                 {
                     return true;
                 }
@@ -57,7 +53,7 @@ namespace Mscee
             }
             if (!vari) // on musta
             {
-                if (uusiY > vanhaY || uusiY - vanhaY < -2)
+                if (uusiY >= vanhaY || uusiY - vanhaY < -2)
                 {
                     return true;
                 }
@@ -65,11 +61,11 @@ namespace Mscee
                 {
                     return true;
                 }
-                if (uusiX == vanhaX && peli.lauta.Lauta(uusiX, uusiY) !=null)
+                if (uusiX == vanhaX && peli.lauta.Lauta(uusiX, uusiY) != null)
                 {
                     return true;
                 }
-                if (uusiY == 4 && peli.lauta.Lauta(uusiX, 5) != null && uusiX - vanhaX == 0)
+                if (uusiY == 4 && peli.lauta.Lauta(uusiX, 5) != null && vanhaY == 6)
                 {
                     return true;
                 }
@@ -77,15 +73,11 @@ namespace Mscee
                 {
                     return true;
                 }
-                if (uusiY - vanhaY == 0)
+                if (uusiX != vanhaX && uusiY - vanhaY == -2)
                 {
                     return true;
                 }
-                if (uusiX - vanhaX != 0 && uusiY - vanhaY == -2)
-                {
-                    return true;
-                }
-                if (uusiX - vanhaX != 0 && peli.lauta.Lauta(uusiX, uusiY) == null)
+                if (uusiX != vanhaX && peli.lauta.Lauta(uusiX, uusiY) == null)
                 {
                     return true;
                 }
@@ -96,36 +88,56 @@ namespace Mscee
 
 
         //// palauttaa truen, jos laiton
-        //public static bool Torni(int vanhaX, int vanhaY, int uusiX, int uusiY)
-        //{
-        //    int siirronpituus = Math.Abs(uusiX - vanhaX);
-        //    if (uusiX - vanhaX != 0 && uusiY - vanhaY != 0)
-        //    {
-        //        return true;
-        //    }
-        //    if (uusiY - vanhaY == 0 && vanhaX < uusiX)
-        //    {
-        //        for (int i = 1 < siirronpituus + 1; i++)
-        //        {
-        //            if (Peli.lauta.Lauta(uusiX + i, uusiY) != null)
-        //            {
-        //                return true;
-        //            }
-        //        }
-        //    }
-        //    if (uusiY - vanhaY == 0 && vanhaX > uusiX)
-        //    {
-        //        for (int i = 1 < siirronpituus + 1; i++)
-        //        {
-        //            if (Peli.lauta.Lauta(uusiX - i, uusiY) != null)
-        //            {
-        //                return true;
-        //            }
-        //        }
-        //    }
+        public static bool Torni(int vanhaX, int vanhaY, int uusiX, int uusiY) // Määritellään tornin laittomat siirrot
+        {
 
-        //    return false;
-        //}
+            if (uusiX - vanhaX != 0 && uusiY - vanhaY != 0)
+            {
+                return true;
+            }
+            if (vanhaX < uusiX) // katsotaan onko reitillä nappuloita, jos on niin palauttaa true
+            {
+                for (int i = vanhaX + 1; i < uusiX; i++)
+                {
+                    if (peli.lauta.Lauta(i, uusiY) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaX > uusiX) // katsotaan onko reitillä nappuloita, jos on niin palauttaa true
+            {
+                for (int i = vanhaX - 1; i > uusiX; i--)
+                {
+                    if (peli.lauta.Lauta(i, uusiY) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaY < uusiY) // nyt kun siirrot tapahtuvat y-akselilla
+            {
+                for (int i = vanhaY + 1; i < uusiY; i++)
+                {
+                    if (peli.lauta.Lauta(uusiX, i) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaY > uusiY) // nyt kun siirrot tapahtuvat y-akselilla
+            {
+                for (int i = vanhaY - 1; i > uusiY; i--)
+                {
+                    if (peli.lauta.Lauta(uusiX, i) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
 
         // palauttaa truen, jos laiton
         public static bool Ratsu(int vanhaX, int vanhaY, int uusiX, int uusiY)
@@ -138,22 +150,172 @@ namespace Mscee
             {
                 return true;
             }
-            if (uusiY - vanhaY == 0 || uusiX - vanhaX == 0)
+            if (uusiY == vanhaY || uusiX == vanhaX)
             {
                 return true;
             }
-            if (uusiX - vanhaX == uusiY - vanhaY)
+            if (uusiX - vanhaX == uusiY - vanhaY || uusiX - vanhaX == -1 * (uusiY - vanhaY))
             {
                 return true;
             }
             return false;
         }
         // palauttaa truen, jos laiton
-        public static bool Lahetti(int vanhaX, int vanhaY, int uusiX, int uusiY) { return false; }
+        public static bool Lahetti(int vanhaX, int vanhaY, int uusiX, int uusiY)
+        {
+            if (uusiX - vanhaX != uusiY - vanhaY && uusiX - vanhaX != -1 * (uusiY - vanhaY))
+            {
+                return true;
+            }
+            if (uusiY > vanhaY && uusiX > vanhaX)
+            {
+                int j = vanhaY + 1;
+                for (int i = vanhaX + 1; i < uusiX; i++, j++)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (uusiY > vanhaY && uusiX < vanhaX)
+            {
+                int j = vanhaY + 1;
+                for (int i = vanhaX - 1; i > uusiX; i--, j++)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (uusiY < vanhaY && uusiX < vanhaX)
+            {
+                int j = vanhaY - 1;
+                for (int i = vanhaX - 1; i > uusiX; i--, j--)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (uusiY < vanhaY && uusiX > vanhaX)
+            {
+                int j = vanhaY - 1;
+                for (int i = vanhaX + 1; i < uusiX; i++, j--)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
         // palauttaa truen, jos laiton
-        public static bool Kuningas(int vanhaX, int vanhaY, int uusiX, int uusiY) { return false; }
+        public static bool Kuningas(int vanhaX, int vanhaY, int uusiX, int uusiY)
+        {
+            if (Math.Abs(uusiX - vanhaX) > 1 || Math.Abs(uusiY - vanhaY) > 1)
+            {
+                return true;
+            }
+            return false;
+        }
         // palauttaa truen, jos laiton
-        public static bool Kuningatar(int vanhaX, int vanhaY, int uusiX, int uusiY) { return false; }
+        public static bool Kuningatar(int vanhaX, int vanhaY, int uusiX, int uusiY)
+        {
+            if (uusiX - vanhaX != 0 && uusiY - vanhaY != 0 && uusiX - vanhaX != uusiY - vanhaY && uusiX - vanhaX != -1 * (uusiY - vanhaY))
+            {
+                return true;
+            }
+            if (uusiY > vanhaY && uusiX > vanhaX)
+            {
+                int j = vanhaY + 1;
+                for (int i = vanhaX + 1; i < uusiX; i++, j++)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (uusiY > vanhaY && uusiX < vanhaX)
+            {
+                int j = vanhaY + 1;
+                for (int i = vanhaX - 1; i > uusiX; i--, j++)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (uusiY < vanhaY && uusiX < vanhaX)
+            {
+                int j = vanhaY - 1;
+                for (int i = vanhaX - 1; i > uusiX; i--, j--)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (uusiY < vanhaY && uusiX > vanhaX)
+            {
+                int j = vanhaY - 1;
+                for (int i = vanhaX + 1; i < uusiX; i++, j--)
+                {
+                    if (peli.lauta.Lauta(i, j) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaX < uusiX && uusiY == vanhaY) // katsotaan onko reitillä nappuloita, jos on niin palauttaa true
+            {
+                for (int i = vanhaX + 1; i < uusiX; i++)
+                {
+                    if (peli.lauta.Lauta(i, uusiY) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaX > uusiX && uusiY == vanhaY) // katsotaan onko reitillä nappuloita, jos on niin palauttaa true
+            {
+                for (int i = vanhaX - 1; i > uusiX; i--)
+                {
+                    if (peli.lauta.Lauta(i, uusiY) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaY < uusiY && uusiX == vanhaX) // nyt kun siirrot tapahtuvat y-akselilla
+            {
+                for (int i = vanhaY + 1; i < uusiY; i++)
+                {
+                    if (peli.lauta.Lauta(uusiX, i) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            if (vanhaY > uusiY && uusiX == vanhaX) // nyt kun siirrot tapahtuvat y-akselilla
+            {
+                for (int i = vanhaY - 1; i > uusiY; i--)
+                {
+                    if (peli.lauta.Lauta(uusiX, i) != null)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
 
         public void Virheteksti(bool Sotilas, bool Torni, bool Ratsu, bool Lahetti, bool Kuningatar, bool Kuningas)
         {
